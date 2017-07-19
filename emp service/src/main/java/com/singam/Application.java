cb24930cbfb47d9f0aaa1ce5e9d42a90984a4851
Bhagya Rupasinghe <brupasinghe@virtusapolaris.com>
2017-07-19 12:05:52 +0530

    zuul route implemented

--- emp service/src/main/java/com/singam/Application.java
+++ emp service/src/main/java/com/singam/Application.java
@@ -3,12 +3,14 @@ package com.singam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Jeyanthasingam on 7/8/2017.
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan({"com.singam", "controller", "service"})
 public class Application {
 
     public static void main(String[] args) {
